02d1597e001d4e68f52c3cf9d5d4a9cd9b5c1f76
Quan Dang Minh <minh.quan040501@gmail.com>
2025-09-03 11:53:41 +0900

    Init project

---
+++ jplearning/src/main/java/com/self/jplearning/JplearningApplication.java
@@ -0,0 +1,13 @@
+package com.self.jplearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JplearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JplearningApplication.class, args);
+	}
+
+}
